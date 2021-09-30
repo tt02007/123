4419700a0116f99c06722f8b3d21096d663eb502
318506 <91247011+318506@users.noreply.github.com>
2021-09-30 16:35:17 +0800

    關燈_有排序的隨機(?)

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
+input.onPinPressed(TouchPin.P0, function () {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            if (y % 2 != 0) {
+                led.unplot(4 - x, 4 - y)
+            } else {
+                led.unplot(x, y)
+            }
+            basic.pause(100)
+        }
+    }
+})
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     for (let x = 0; x <= 4; x++) {
         for (let y = 0; y <= 4; y++) {
             led.plot(x, y)
+            basic.pause(100)
+        }
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            if (x % 2 != 0) {
+                led.plot(x, 4 - y)
+            } else {
+                led.plot(x, y)
+            }
             basic.pause(100)
         }
     }
@@ -20,7 +45,7 @@ input.onButtonPressed(Button.B, function () {
         }
     }
 })
-basic.pause(5000)
+basic.pause(100)
 for (let y = 0; y <= 4; y++) {
     for (let x = 0; x <= 4; x++) {
         led.plot(x, y)
